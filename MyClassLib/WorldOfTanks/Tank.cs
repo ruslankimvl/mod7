e01998d698cfdd4d87c56e0bb9232d25d86e2e7b
Ruslan Kim <kimryslan2003@gmail.com>
2019-03-07 17:30:29 +0600

    Добавьте файлы проекта.

---
+++ MyClassLib/WorldOfTanks/Tank.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyClassLib.WorldOfTanks
+{
+    class Tank
+    {
+        protected string name; // Название
+        protected int ammo; // Боекомплект
+        protected int armor; // Броня
+        protected int mobility; // Манёвренность
+        public Tank(int ammo, int armor, int mobility, string name)
+        {
+            this.ammo = ammo;
+            this.armor = armor;
+            this.mobility = mobility;
+            this.name = name;
+        }
+        public Tank()
+        {
+            ammo = 0;
+            armor = 0;
+            mobility = 0;
+            name = "null";
+        }
+        public void Print()
+        {
+            Console.WriteLine("Name - {0}", name);
+            Console.WriteLine("Ammunation = {0} Armor = {0} Mobility = {0}", ammo, armor, mobility);
+        }
+
+        public static Tank operator *(Tank T34, Tank pantera)
+        {
+            if (T34.ammo > pantera.ammo && T34.armor > pantera.armor)
+                return T34;
+
+            if (T34.ammo > pantera.ammo && T34.mobility > pantera.mobility)
+                return T34;
+
+            if (T34.armor > pantera.armor && T34.mobility > pantera.mobility)
+                return T34;
+            else
+                return pantera;
+        }
+    }
+}
---
+++ MyClassLib/program.cs
@@ -0,0 +1,44 @@
+﻿using MyClassLib.WorldOfTanks;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyClassLib
+{
+    public class program
+    {
+        static void Main(string[] args)
+        {
+            Random rand = new Random();
+            Tank[] VIN = new Tank[5];
+            Tank[] T34 = new Tank[5];
+            for (int i = 0; i < T34.Length; i++)
+            {
+                VIN[i] = new Tank();
+            }
+            for (int i = 0; i < T34.Length; i++)
+            {
+                T34[i] = new Tank(rand.Next(0, 100), rand.Next(0, 100), rand.Next(0, 100), "T34");
+            }
+            Tank[] pantera = new Tank[5];
+            for (int i = 0; i < T34.Length; i++)
+            {
+                pantera[i] = new Tank(rand.Next(0, 100), rand.Next(0, 100), rand.Next(0, 100), "pantera");
+            }
+            for (int i = 0; i < 5; i++)
+            {
+                T34[i].Print();
+                pantera[i].Print(); ;
+                VIN[i] = T34[i] * pantera[i];
+                Console.WriteLine();
+                Console.WriteLine($"В {i + 1} схватке победил");
+                VIN[i].Print();
+                Console.WriteLine();
+                Console.WriteLine();
+            }
+
+        }
+    }
+}
